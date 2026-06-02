51f59a129428a7525dd866472721da4e855a9c4b
uijyl <uijyl@student.kit.edu>
2026-06-02 21:31:42 +0200

    Movement und Input Split

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(EntityMovement))] // Zwingt Unity dazu, das Movement-Skript am gleichen Objekt zu haben
+public class PlayerInputController : MonoBehaviour
+{
+    private EntityMovement moveScript;
+
+    void Awake()
+    {
+        // Wir suchen uns den Motor, der an diesem Objekt hängt
+        moveScript = GetComponent<EntityMovement>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // 1. Tastatur-Eingaben an den Motor senden
+        moveScript.inputX = Input.GetAxisRaw("Horizontal");
+        moveScript.inputY = Input.GetAxisRaw("Vertical");
+        moveScript.inputJumping = Input.GetButton("Jump");
+        moveScript.inputCrouching = Input.GetKey(KeyCode.LeftControl);
+
+        // 2. Maus-Eingaben an den Motor senden
+        moveScript.inputMouseX = Input.GetAxis("Mouse X");
+        moveScript.inputMouseY = Input.GetAxis("Mouse Y");
+
+        // 3. Wenn die Ducken-Taste genau JETZT gedrückt oder losgelassen wird, 
+        // rufen wir die Methoden im Motor auf.
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            moveScript.StartCrouch();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            moveScript.StopCrouch();
+        }
+    }
+}
